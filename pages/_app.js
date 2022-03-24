1d6e19cdd6d5a841ee0f7018fd7e58e0fe256417
Alejandro Cazorla <acazorla72@gmail.com>
2022-03-24 20:36:23 +0100

    added conditional on load

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import React, { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  const [isShowingModal, setShowingModal] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("load", setShowingModal(true));
+  }, []);
+
+  if (isShowingModal) {
+    //
+    return (
+      <div>
+        <h2>Here my Modal is active</h2>
+        <button onClick={() => setShowingModal(false)}>
+          With this button we desactivate it
+        </button>
+        <button>With this one we do not</button>
+      </div>
+    );
+    //
+  } else if (!isShowingModal) {
+    //
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 
 export default MyApp;
